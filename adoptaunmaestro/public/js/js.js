175b420e6cf3c84fe68156503830d6c951d23a97
jcfinoquio <jcfinoquio@gmail.com>
2025-10-11 19:06:25 +0200

    mejora pagina login

--- adoptaunmaestro/public/js/js.js
+++ adoptaunmaestro/public/js/js.js
@@ -1,1 +1,10 @@
-// Código JavaScript inicial 
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  const loginContainer = document.querySelector(".login-container");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault(); // evita recargar
+    loginContainer.style.display = "none"; // oculta el lightbox
+    document.body.style.overflow = "auto"; // vuelve a permitir scroll
+  });
+});
